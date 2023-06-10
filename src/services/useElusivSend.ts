2897438fccf65cbad5887e388f9de29eb761df4a
Huy Nguyen Quang <39359294+quanghuynguyen1902@users.noreply.github.com>
2023-06-11 00:09:21 +0700

    feat: add elusiv service

---
+++ src/services/useElusivSend.ts
@@ -0,0 +1,21 @@
+import { useQuery } from 'react-query';
+import useElusivInstance from './useElusivInstance';
+import { TokenType } from '@elusiv/sdk';
+import {PublicKey} from "@solana/web3.js";
+
+function useElusivSend(amount: number, recipient: PublicKey, tokenType: TokenType) {
+    const { data: elusivInstance } = useElusivInstance();
+    return useQuery(
+        ['elusiv-send', recipient],
+        async () => {
+            const sendTx = await elusivInstance?.buildSendTx(amount, recipient, tokenType);
+            // Sign it (only needed for topups, as we're topping up from our public key there)
+            return elusivInstance?.sendElusivTx(sendTx!);
+        },
+        {
+            enabled: !!elusivInstance && !!recipient,
+        }
+    );
+}
+
+export default useElusivSend;
